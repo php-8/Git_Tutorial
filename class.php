c0814c4de7091f2208202f25f4c82e7ce2c7e80a
Michael <melnikov.vsevolod@yandex.ru>
2019-04-20 17:46:26 +0300

    Master commit

--- class.php
+++ class.php
@@ -2,6 +2,16 @@
 
 class NewClass {
 
+public $some;
+public $thing;
+
+public function __construct($some, $thing) {
+
+$this->some = $some;
+$this->thing = $thing;
+
+}
+
 public function NewFunction () {
 
 return 'Something';
